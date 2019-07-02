0d7046cd5ed33da4165b30d12f24bb41de8ef844
Arjun <denverbdr@protonmail.com>
2019-07-02 10:44:06 -0700

    Fix icon

--- res/resize.sh
+++ res/resize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for size in 16 32 48 128 256; do
-    inkscape -z -e $size.png -w $size -h $size logo2.svg >/dev/null 2>/dev/null
+    inkscape -z -e $size.png -w $size -h $size -y 0 logo2.svg >/dev/null 2>/dev/null
 done
 convert 16.png 32.png 48.png 128.png 256.png -colors 256 icon.ico
 rm 16.png 32.png 48.png 128.png 256.png 
