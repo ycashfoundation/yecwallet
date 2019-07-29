z-bitcoinz/BitcoinZ_Blue

Support fast sync during initial block download

src/connection.cpp
@@ -185,9 +185,18 @@ void ConnectionLoader::createZcashConf() {
     out << "addnode=mainnet.z.cash\n";
     out << "rpcuser=zec-qt-wallet\n";
     out << "rpcpassword=" % randomPassword() << "\n";
+
+    // Fast sync override
+    if (ui.chkFastSync->isChecked()) {
+        out << "ibdskiptxverification=1\n";
+    }
+
+    // Datadir override 
     if (!datadir.isEmpty()) {
         out << "datadir=" % datadir % "\n";
     }
+
+    // Tor override
     if (useTor) {
         out << "proxy=127.0.0.1:9050\n";
     }
@@ -623,6 +632,9 @@ std::shared_ptr<ConnectionConfig> ConnectionLoader::autoDetectZcashConf() {
             zcashconf->port.isEmpty()) {
                 zcashconf->port = "18232";
         }
+        if (name == "ibdskiptxverification" && value == "1") {
+            zcashconf->skiptxverification = true;
+        }
     }
 
     // If rpcport is not in the file, and it was not set by the testnet=1 flag, then go to default
@@ -649,7 +661,7 @@ std::shared_ptr<ConnectionConfig> ConnectionLoader::loadFromSettings() {
     if (username.isEmpty() || password.isEmpty())
         return nullptr;
 
-    auto uiConfig = new ConnectionConfig{ host, port, username, password, false, false, "", "", ConnectionType::UISettingsZCashD};
+    auto uiConfig = new ConnectionConfig{ host, port, username, password, false, false, false, "", "", ConnectionType::UISettingsZCashD};
 
     return std::shared_ptr<ConnectionConfig>(uiConfig);
 }

src/rpc.cpp
@@ -635,6 +635,14 @@ void RPC::getInfoThenRefresh(bool force) {
                     ui->blockheight->setText(txt);
                     ui->heightLabel->setText(QObject::tr("Downloading blocks"));
                 } else {
+                    // If syncing is finished, we may have to remove the ibdskiptxverification
+                    // flag from zcash.conf
+                    if (getConnection() != nullptr && getConnection()->config->skiptxverification) {
+                        getConnection()->config->skiptxverification = false;
+                        Settings::removeFromZcashConf(Settings::getInstance()->getZcashdConfLocation(), 
+                                                        "ibdskiptxverification");
+                    }
+
                     ui->blockheight->setText(QString::number(blockNumber));
                     ui->heightLabel->setText(QObject::tr("Block height"));
                 }

ycashfoundation/yecwallet

src/connection.cpp
@@ -193,9 +193,18 @@ void ConnectionLoader::createZcashConf() {
     out << "addnode=main4.ycash.xyz\n";
     out << "rpcuser=ycash\n";
     out << "rpcpassword=" % randomPassword() << "\n";
+
+    // Fast sync override
+    if (ui.chkFastSync->isChecked()) {
+        out << "fastsync=1\n";
+    }
+
+    // Datadir override 
     if (!datadir.isEmpty()) {
         out << "datadir=" % datadir % "\n";
     }
+
+    // Tor override
     if (useTor) {
         out << "proxy=127.0.0.1:9050\n";
     }
@@ -634,6 +643,9 @@ std::shared_ptr<ConnectionConfig> ConnectionLoader::autoDetectZcashConf() {
             zcashconf->port.isEmpty()) {
                 zcashconf->port = "18832";
         }
+        if (name == "fastsync" && value == "1") {
+            zcashconf->fastsync = true;
+        }
     }
 
     // If rpcport is not in the file, and it was not set by the testnet=1 flag, then go to default
@@ -660,7 +672,7 @@ std::shared_ptr<ConnectionConfig> ConnectionLoader::loadFromSettings() {
     if (username.isEmpty() || password.isEmpty())
         return nullptr;
 
-    auto uiConfig = new ConnectionConfig{ host, port, username, password, false, false, "", "", ConnectionType::UISettingsZCashD};
+    auto uiConfig = new ConnectionConfig{ host, port, username, password, false, false, false, "", "", ConnectionType::UISettingsZCashD};
 
     return std::shared_ptr<ConnectionConfig>(uiConfig);
 }

src/rpc.cpp
@@ -640,6 +640,14 @@ void RPC::getInfoThenRefresh(bool force) {
                     ui->blockheight->setText(txt);
                     ui->heightLabel->setText(QObject::tr("Downloading blocks"));
                 } else {
+                    // If syncing is finished, we may have to remove the ibdskiptxverification
+                    // flag from zcash.conf
+                    if (getConnection() != nullptr && getConnection()->config->fastsync) {
+                        getConnection()->config->fastsync = false;
+                        Settings::removeFromZcashConf(Settings::getInstance()->getZcashdConfLocation(), 
+                                                        "ibdskiptxverification");
+                    }
+
                     ui->blockheight->setText(QString::number(blockNumber));
                     ui->heightLabel->setText(QObject::tr("Block height"));
                 }